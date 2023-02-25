69ba625b80a6b7e7ba29914814c5b2f6dcad922d
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-02-25 18:09:18 -0300

    erro

--- Projetos/sabados_letivos/bin/sabados_letivos.dart
+++ Projetos/sabados_letivos/bin/sabados_letivos.dart
@@ -1,5 +1,7 @@
 import 'package:sabados_letivos/sabados_letivos.dart' as sabados_letivos;
 
+
 void main(List<String> arguments) {
-  print('Hello world: ${sabados_letivos.calculate()}!');
+
+  calcularSalario(50,60);
 }
--- Projetos/sabados_letivos/lib/sabados_letivos.dart
+++ Projetos/sabados_letivos/lib/sabados_letivos.dart
@@ -1,3 +1,7 @@
 int calculate() {
   return 6 * 7;
 }
+
+int calcularSalario(v_salario, v_desconto) {
+  return (v_salario + v_desconto);
+}
