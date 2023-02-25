94ddb80db87bd8ef42232b865e02ba1617e8f859
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-02-25 17:52:04 -0300

    Atidades do sabado letivo

---
+++ Projetos/sabados_letivos/desconto_salario/bin/desconto_salario.dart
@@ -0,0 +1,5 @@
+import 'package:desconto_salario/desconto_salario.dart' as desconto_salario;
+
+void main(List<String> arguments) {
+  print('Hello world: ${desconto_salario.calculate()}!');
+}
---
+++ Projetos/sabados_letivos/desconto_salario/test/desconto_salario_test.dart
@@ -0,0 +1,8 @@
+import 'package:desconto_salario/desconto_salario.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
