2f19acaac2100cb45b44bbece35a82f764b53b90
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-02-25 17:50:21 -0300

    teste

--- Projetos/atividades de dart/bin/solicitanto_valores.dart
+++ Projetos/atividades de dart/bin/solicitanto_valores.dart
@@ -6,32 +6,28 @@ import 'dart:io';
 void main(List<String> arguments) {
   produto();
 
-
 /*Atividade 03 - ao invés de atribuir valores solicite dados
 String para int -> int.parse
 String para double -> double.parse
 int para string -> variavel.toString()
 */
-  
 }
 
- produto(){
-    print('informe o modelo: ');
-    String modelo = stdin.readLineSync()!;
-    print('informe a ram: ');
-    int ram = int.parse(stdin.readLineSync()!);
-    print('informe o submodelo: ');
-    String submodelo = stdin.readLineSync()!;
-    print('informe o armazenamento: ');
-    int armazenamento = int.parse(stdin.readLineSync()!);
-      print('informe a cor: ');
-    String cor = stdin.readLineSync()!;
+produto() {
+  print('informe o modelo: ');
+  String modelo = stdin.readLineSync()!;
+  print('informe a ram: ');
+  int ram = int.parse(stdin.readLineSync()!);
+  print('informe o submodelo: ');
+  String submodelo = stdin.readLineSync()!;
+  print('informe o armazenamento: ');
+  int armazenamento = int.parse(stdin.readLineSync()!);
+  print('informe a cor: ');
+  String cor = stdin.readLineSync()!;
 
-    print(modelo);
-    print(ram);
-    print(submodelo);
-    print(armazenamento);
-    print(cor);
-
-  }
-  
+  print(modelo);
+  print(ram);
+  print(submodelo);
+  print(armazenamento);
+  print(cor);
+}
