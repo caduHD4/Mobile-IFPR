489e9628a3087f943fac4dc08949ee6d57711529
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-02-26 21:58:41 -0300

    Implementando calculadora

--- Projetos/calcuflistone/bin/calcuflistone.dart
+++ Projetos/calcuflistone/bin/calcuflistone.dart
@@ -1,5 +1,8 @@
 import 'package:calcuflistone/calcuflistone.dart' as calcuflistone;
 
-void main(List<String> arguments) {
-  print('Hello world: ${calcuflistone.calculate()}!');
+import 'dart:io';
+
+void main() {
+  
 }
+
