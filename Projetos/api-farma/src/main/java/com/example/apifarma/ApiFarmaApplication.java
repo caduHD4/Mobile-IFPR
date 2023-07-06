eacc5bb0d5d1d3f4506da5d98289064a9ec74a5e
caduHD4 <cadumarianogarciapereira@gmail.com>
2023-07-06 20:51:33 -0300

    Criação da API

---
+++ Projetos/api-farma/src/main/java/com/example/apifarma/ApiFarmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apifarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFarmaApplication.class, args);
+	}
+
+}
